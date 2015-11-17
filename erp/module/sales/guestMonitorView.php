f91856a114b39f4b44859fe995996a86c935a60f
eepletnev <ee.pletnev@me.com>
2015-11-17 16:10:29 +0300

    Guest Monitor

--- erp/module/sales/guestMonitorView.php
+++ erp/module/sales/guestMonitorView.php
@@ -22,10 +22,10 @@ echo "–––––––<br>";
 foreach ($numbers as $month => $clients) {
 	if ($clients['registred'] != 0 || $clients['not'] != 0) {
 		$loyalityStat = $clients['loyalityLevel'];
-		echo date("F", mktime(0, 0, 0, $month, 1, 2015)) . ": " . number_format(($clients['registred'] / ($clients['not'] + $clients['registred']))*100) . "%<br>";
+		echo date("F", mktime(0, 0, 0, $month, 1, 2015)) . ": " . number_format(($clients['registred'] / ($clients['not'] + $clients['registred']))*100, 2) . "%<br>";
 		echo "<br>";
 		$total = array_sum($loyalityStat);
-		echo number_format($loyalityStat['low']/$total * 100, 2) . "% / " . number_format($loyalityStat['average']/$total * 100) . "% / " . number_format($loyalityStat['huge']/$total * 100) . "%<br>";
+		echo number_format($loyalityStat['low']/$total * 100, 2) . "% / " . number_format($loyalityStat['average']/$total * 100, 2) . "% / " . number_format($loyalityStat['huge']/$total * 100, 2) . "%<br>";
 		echo "–––––––<br>";	
 	}
 
@@ -35,3 +35,8 @@ foreach ($numbers as $month => $clients) {
 
 echo "<b>" . $monitor->getAverageClientLife($mysqli) . " дней</b>  – средняя продолжительность жизни нашего клиента. C:";
 echo "<br>";
+
+?>
+<pre>
+
+</pre>
